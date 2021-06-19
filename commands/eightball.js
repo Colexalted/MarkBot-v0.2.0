cbf3347e0fc7a3fba476ea7a96a5c5814332bdca
ItsCob <60833493+Colexalted@users.noreply.github.com>
2021-06-19 07:18:00 -0400

    8ball command updates

--- commands/eightball.js
+++ commands/eightball.js
@@ -8,8 +8,40 @@ module.exports.config = {
 }
 
 module.exports.run = async (client, message, args) => {
-let eightball = ['Yes', 'No', 'Maybe', "I'm not sure, ask again later"]
+let eightball = ['Yes', 'No', 'Maybe', "I'm not sure, ask again later", "Sorry homie, thats classified :wink:", "If you think so...", "Just a tad", "You don't want to know.", "Nah", "I don't wanna tell you :angry:"]
 let eightball1 = Math.floor(Math.random() * eightball.length)
 message.channel.send(eightball[eightball1])
 
-}
+//Commented out the below code because it isn't working yet... ;-;
+
+
+
+/*
+channel.messages.fetch({ limit: 2 }).then(bruh=> {
+    
+let lastMessage = bruh.first()
+let sentBefore = bruh.last()
+
+  
+if(!lastMessage.author.client)
+{
+        if(lastMessage.author = sentBefore.author)
+        {
+        
+            if(lastMessage.content == sentBefore.content) 
+            {
+             message.channel.send("stop asking bro")
+            }
+        }
+        
+ }
+
+})   )
+*/
+
+
+
+
+
+
+}    
