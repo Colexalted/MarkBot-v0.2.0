4f4b83ed90fe8b9c280302c2cd2cfd6cae09e9df
ItsCob <60833493+Colexalted@users.noreply.github.com>
2021-09-02 09:50:36 -0700

    lightning mcqueen update

--- commands/pickup.js
+++ commands/pickup.js
@@ -15,7 +15,7 @@ module.exports.run = async (client, message, args) => {
     
 
 
-    let joe = ["You’re so hot, my zipper is falling for you.", "I’m on top of things. Would you like to be one of them?", "I’m not into watching sunsets, but I’d love to see you go down.", "Your outfit would look great on my bedroom floor.", "I lost my keys… can I check your pants?",  "Are you my homework? Because I’m not doing you, but I definitely should be.", "If you were a flower, you’d be a damn-delion.","Is that a mirror in your pants because I can see myself in them.", "If you look that good in clothes, you must look even better out of them."]
+    let joe = ["You’re so hot, my zipper is falling for you.", "I’m on top of things. Would you like to be one of them?", "I’m not into watching sunsets, but I’d love to see you go down.", "Your outfit would look great on my bedroom floor.", "I lost my keys… can I check your pants?",  "Are you my homework? Because I’m not doing you, but I definitely should be.", "If you were a flower, you’d be a damn-delion.","Is that a mirror in your pants because I can see myself in them.", "If you look that good in clothes, you must look even better out of them.", "Are you lightning McQueen, cuz you give me a rock hard boner", "Are you google because you're everything I'm searching for."]
     
     let pickup2 = Math.floor(Math.random() * joe.length)
 
